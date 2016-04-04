f3c5ab4562891193b273e8d070f464d9dc1b2add
SarahRebeccaCox <sarah.cox@nyu.edu>
2016-04-04 19:26:16 -0400

    Cleaned up
    Debugged and cleaned

--- LearningCurve.R
+++ LearningCurve.R
@@ -1,88 +1,110 @@
 data <- read.csv("cleanerData.csv")
 sort(data)
 
-data2 <- data[with(data, order(skill_id, user_id)), ]
-data2[10000:10100,]
+length(unique(data$skill_id))
 
 
-skill1 <- data[data$skill_id==1,]
-skill1 <- skill1[is.na(skill1$user_id)==FALSE,]
-dim(skill1)
 
-skill1 <- skill1[with(skill1, order(user_id)),]
-skill1.vis <- skill1[,c(3,7)]
-
-#create new data frame for visualizing
-learning.curve.df <- data.frame(matrix(c(0,0,0,0,0,0),2,3))
-names(learning.curve.df) <- c("Student.ID","Attempts.to.Correct","Try.Number")
-learning.curve.df[,1] <- skill1$user_id[1] #initiate
-
-
-########THIS DATAFRAME WILL EXCLUDE STUDENTS WHO DO NOT ANSWER ANY QUESTION INCORRECTLY
-learn.curve.df.row <- 3 #initialize row
-for (i in 3:nrow(skill1)){ #just manually do 1 and 2
-  
-  #FOR NEW USERS
-  if (skill1$user_id[i] != skill1$user_id[i-1]){ #if it's a new user
-    count <- 0 #reset the count
-    if (skill1$correct[i]==0){ #if they got the question wrong
-      count <- count+1 #add one to their attempt count
-    }
-    if (skill1$correct[i]==1){ #when they get it right 
-      if (count != 0){ #if it's not the second or third in a row 
-        learning.curve.df[learn.curve.df.row,1] <- skill1$user_id[i] #fill in user id 
-        learning.curve.df[learn.curve.df.row,2] <- count #record the count
-        learn.curve.df.row <- learn.curve.df.row + 1 #move to the next recording row
-      }
-      count <- 0 #reset the count
-    }
-  } #end of loop
-  
-  #FOR RETURNING USERS
-  if (skill1$user_id[i] == skill1$user_id[i-1]){ #if it's the same user
-    if (skill1$correct[i]==0){ #if they got the question wrong
-      count <- count+1 #add one to their attempt count
-    }
-    if (skill1$correct[i]==1){ #when they get it right 
-      if (count != 0){ #if it's not the second or third in a row 
-        learning.curve.df[learn.curve.df.row,1] <- skill1$user_id[i] #fill in user id 
-        learning.curve.df[learn.curve.df.row,2] <- count #record the count
-        learn.curve.df.row <- learn.curve.df.row + 1 #move to the next recording row
-      }
-      count <- 0 #reset the count
-    }
-  }
+split.by.skill <- function(skill.id){
+  assign("x",data[data$skill_id==skill.id,])
+  x <- x[is.na(x$user_id)==FALSE,]
+  x <- x[,c(3,7)]
+  x <- x[order(x$user_id),]
+  return(x)
 }
 
 
-#filling in try number
-for (i in 1:nrow(learning.curve.df)){
-  if (i == 1){ #starter exception
-    count <- 1
-    learning.curve.df$Try.Number[i] <- count
-    next
+#####This function outputs plots that show how many questions
+#####A student answers incorrectly between each correct answer
+#####Until they "master" the skill or quit
+#####Students who only have correct answers are not shown in the plots
+learning.curve <- function(skill.id){
+  
+  x <- assign(paste0("skill",as.character(skill.id)),split.by.skill(skill.id)) #skill-specific dataframe
+
+  #create new data frame for visualizing
+  learning.curve.df <- data.frame(matrix(c(0,0,0,0,0,0),2,3))
+  names(learning.curve.df) <- c("Student.ID","Attempts.to.Correct","Try.Number")
+  
+  learn.curve.df.row <- 1 #initialize row
+  for (i in 1:nrow(x)){ 
+    
+    #FOR THE FIRST ROW
+    if (i == 1){ 
+      count <- 0
+      next
+    }
+    
+    #FOR NEW USERS
+    if (x$user_id[i] != x$user_id[i-1]){ #if it's a new user
+      count <- 0 #reset the count
+      if (x$correct[i]==0){ #if they got the question wrong
+        count <- count+1 #add one to their attempt count
+      }
+      if (x$correct[i]==1){ #when they get it right 
+        if (count != 0){ #if it's not the second or third in a row 
+          learning.curve.df[learn.curve.df.row,1] <- x$user_id[i] #fill in user id 
+          learning.curve.df[learn.curve.df.row,2] <- count #record the count
+          learn.curve.df.row <- learn.curve.df.row + 1 #move to the next recording row
+        }
+        count <- 0 #reset the count
+      }
+    } #end of loop
+    
+    #FOR RETURNING USERS
+    if (x$user_id[i] == x$user_id[i-1]){ #if it's the same user
+      if (x$correct[i]==0){ #if they got the question wrong
+        count <- count+1 #add one to their attempt count
+      }
+      if (x$correct[i]==1){ #when they get it right 
+        if (count != 0){ #if it's not the second or third in a row 
+          learning.curve.df[learn.curve.df.row,1] <- x$user_id[i] #fill in user id 
+          learning.curve.df[learn.curve.df.row,2] <- count #record the count
+          learn.curve.df.row <- learn.curve.df.row + 1 #move to the next recording row
+        }
+        count <- 0 #reset the count
+      }
+    }
   }
-  if (learning.curve.df$Student.ID[i] != learning.curve.df$Student.ID[i-1]){ #if new user
-    count <- 1 #reset count
-    learning.curve.df$Try.Number[i] <- count #fill in row
+  
+  #filling in try number
+  for (j in 1:nrow(learning.curve.df)){
+    if (j == 1){ #starter exception
+      count <- 1
+      learning.curve.df$Try.Number[j] <- count
+      next
+    }
+    if (learning.curve.df$Student.ID[j] != learning.curve.df$Student.ID[j-1]){ #if new user
+      count <- 1 #reset count
+      learning.curve.df$Try.Number[j] <- count #fill in row
+    }
+    if (learning.curve.df$Student.ID[j] == learning.curve.df$Student.ID[j-1]){ #if returning user
+      count <- count + 1 #add to count
+      learning.curve.df$Try.Number[j] <- count #fill in row
+    }
   }
-  if (learning.curve.df$Student.ID[i] == learning.curve.df$Student.ID[i-1]){ #if returning user
-    count <- count + 1 #add to count
-    learning.curve.df$Try.Number[i] <- count #fill in row
-  }
+  
+
+  ###Visualization
+
+  plot <- ggplot(learning.curve.df, aes(Try.Number, Attempts.to.Correct,colour=Student.ID)) + 
+    geom_line(aes(group = Student.ID)) + 
+    geom_point() + 
+    labs(title=paste0("Skill ",as.character(skill.id))) +
+    labs(x="Try Number") +
+    labs(y="Questions Wrong Before a Correct")
+  
+  return(plot)
+
 }
-##########
-#PLOTTING#
-##########
 
-library(ggplot2)
 
-skill1.plot <- ggplot(learning.curve.df, aes(Try.Number, Attempts.to.Correct,colour=Student.ID)) + 
-  geom_line(aes(group = Student.ID)) + 
-  geom_point() + 
-  labs(title="Skill 1") +
-  labs(x="Try Number") +
-  labs(y="Questions Wrong Before a Correct")
+
+unique(data$skill_id) #see the list of skill ID's
+
+learning.curve(1) #plot for skill 1
+learning.curve(2) #etc
+learning.curve(4)
 
 
 
